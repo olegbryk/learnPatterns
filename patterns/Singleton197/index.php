fe8ad33de9fbc097809ae8b4d4a5864f4218917d
Oleg Bryk <olegbrykmail@gmail.com>
2018-01-15 16:07:21 +0200

    first classes

---
+++ patterns/Singleton197/index.php
@@ -0,0 +1,42 @@
+<h1>
+    Singleton
+</h1>
+<?php
+abstract class lessons{
+    public function _constructor(){
+
+}
+public function cost(){
+
+}
+
+public function chargeType(){
+
+}
+}
+class  FixedPriceLesson extends lessons
+{
+    public function cost()
+    {
+
+    }
+
+    public function chargeType()
+    {
+
+    }
+}
+
+class  TimedPriceLesson extends lessons
+{
+    public function cost()
+    {
+
+    }
+
+    public function chargeType()
+    {
+
+    }
+}
+?>
