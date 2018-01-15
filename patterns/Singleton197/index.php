6a401d15ec0be30b15574501051737ef353f1dfd
Oleg Bryk <olegbrykmail@gmail.com>
2018-01-15 16:57:18 +0200

    code sample 184

--- patterns/Singleton197/index.php
+++ patterns/Singleton197/index.php
@@ -2,41 +2,56 @@
     Singleton
 </h1>
 <?php
-abstract class lessons{
-    public function _constructor(){
-
-}
-public function cost(){
-
-}
-
-public function chargeType(){
-
-}
-}
-class  FixedPriceLesson extends lessons
-{
-    public function cost()
-    {
+abstract class lesson{
+    protected $duration;
+    const FIXED =1;
+    const TIMED =2;
+    private $costtype;
+    public function __construct ($duration, $costtype = self::FIXED){
+        $this->duration = $duration;
+        $this->costtype = $costtype;
 
     }
+    public function cost(){
+            switch ($this->costtype){
+                CASE self::TIMED :
+                    return (5 *$this->duration);
+                    break;
+                CASE self::FIXED:
+                    return 30;
+                    break;
+                default:
+                    $this->costtype = self::FIXED;
+                    return 30;
+            }
+    }
 
-    public function chargeType()
-    {
+    public function chargeType(){
+        switch ($this->costtype){
+            CASE self::TIMED:
+                return "Погодинна оплата";
+                break;
+            break;
+            CASE self::FIXED:
+                return "Фіксована оплата";
+                break;
+            default:
+                $this->costtype = self::FIXED;
+                return "Фіксована оплата";
 
+        }
     }
 }
 
-class  TimedPriceLesson extends lessons
-{
-    public function cost()
-    {
+class Lecture extends Lesson{
 
-    }
-
-    public function chargeType()
-    {
-
-    }
 }
+
+class Seminar extends Lesson{
+
+}
+$lecture = new Lecture(5,Lesson::FIXED);
+$seminar = new Seminar(3,Lesson::TIMED);
+print "{$lecture->cost()} ({$lecture->chargeType()})/<br/>";
+print "{$seminar->cost()} ({$seminar->chargeType()})/<br/>";
 ?>
